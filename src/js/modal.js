e920c130abbde2e58c0a83314b0b34c3ef05fd72
VasilyMalashkin <vasilym5@yandex.ru>
2019-08-26 19:05:43 +0400

    Написан скрипт отправки формы на сервер через Ajax.

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
   var button = $('#button');
   var modal = $('#modal');
   var close = $('#close');
+  var cardLink = $('.card__link');
 
   button.on('click', function(){
+    modal.addClass('modal_active');
+  })
+
+  cardLink.on('click', function (event) {
+    event.preventDefault();
     modal.addClass('modal_active');
   })
 
---
+++ src/mail.php
@@ -0,0 +1,5 @@
+<?php
+$data = $_POST;
+echo $data['username'];
+
+/*echo json_encode($data,JSON_UNESCAPED_UNICODE);*/
