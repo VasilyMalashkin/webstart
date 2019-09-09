f15a23597e8d5000990a7f2198397296eb26de6a
VasilyMalashkin <vasilym5@yandex.ru>
2019-09-09 18:55:56 +0400

    Настройка Gulp и оптимизация сайта.

---
+++ dist/js/modal.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  var button = $('#button');
+  var modal = $('#modal');
+  var close = $('#close');
+  var cardLink = $('.card__link');
+
+  button.on('click', function(){
+    modal.addClass('modal_active');
+  })
+
+  cardLink.on('click', function (event) {
+    event.preventDefault();
+    modal.addClass('modal_active');
+  })
+
+  close.on('click', function(){
+    modal.removeClass('modal_active');
+  });
+});
